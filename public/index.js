7ce9cfb06f6c5fcd6e05837ebe0b5a41375664cc
skolaracc <132059503+skolaracc@users.noreply.github.com>
2023-05-05 13:16:04 +0200

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,141 @@
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  query,
+  getDocs,
+  where,
+  orderBy,
+  setDoc,
+  doc,
+  deleteDoc,
+} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzZrHZ-GDUlOa9OAA4GQutmGSkkJN_e78",
+  authDomain: "projekatt-6a688.firebaseapp.com",
+  projectId: "projekatt-6a688",
+  storageBucket: "projekatt-6a688.appspot.com",
+  messagingSenderId: "106158181819",
+  appId: "1:106158181819:web:19f006d418830aac1b889e",
+  measurementId: "G-W1E7FBNFC7",
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+var provider;
+var auth;
+var credential;
+var token;
+
+var email;
+
+window.onload = function () {
+  document.getElementById("loginbutton").onclick = function () {
+    signIn();
+  };
+  document.getElementById("createitembutton").onclick = function () {
+    createShoppingListDocument();
+  };
+};
+
+async function signIn() {
+  provider = new GoogleAuthProvider();
+  auth = getAuth();
+
+  signInWithPopup(auth, provider).then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    credential = GoogleAuthProvider.credentialFromResult(result);
+    token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    email = user.email;
+    document.getElementById("useremail").innerHTML = email;
+
+    document.getElementById("loginbutton").style.display = "none";
+    document.getElementById("shoppinglistpage").style.display = "block";
+
+    displayShoppingList(email);
+  });
+}
+
+async function displayShoppingList(email) {
+  // retrieve the shoppingList documents for email and turn them into entries
+  // in an editable Shopping List table
+
+  let userShoppingList = "";
+  const userShoppingListsCollection = collection(db, "userShoppingLists");
+  const userShoppingListsQuery = query(
+    userShoppingListsCollection,
+    where("userEmail", "==", email),
+    orderBy("userPurchase", "asc")
+  );
+  const userShoppingListsSnapshot = await getDocs(userShoppingListsQuery);
+
+  userShoppingListsSnapshot.forEach(function (doc) {
+    userShoppingList +=
+      `
+        <input type='text' maxlength='30' size='20' id='o` +
+      doc.id +
+      `' autocomplete='off'
+            placeholder='` +
+      doc.data().userPurchase +
+      `'
+            value='` +
+      doc.data().userPurchase +
+      `'>
+            <button id =  'e` +
+      doc.id +
+      `'>Update</button>
+            <button id =  'd` +
+      doc.id +
+      `'>Delete</button><br>
+            `;
+  });
+
+  document.getElementById("usershoppinglist").innerHTML = userShoppingList;
+  userShoppingListsSnapshot.forEach(function (doc) {
+    document.getElementById("e" + doc.id).onclick = function () {
+      updateShoppingListDocument(doc.id);
+    };
+    document.getElementById("d" + doc.id).onclick = function () {
+      deleteShoppingListDocument(doc.id);
+    };
+  });
+}
+
+async function updateShoppingListDocument(id) {
+  // update the userPurchase field for document id
+
+  let newUserPurchase = document.getElementById("o" + id).value;
+  const docRef = doc(db, "userShoppingLists", id);
+  await setDoc(docRef, { userPurchase: newUserPurchase }, { merge: true });
+}
+
+async function deleteShoppingListDocument(id) {
+  // delete the document for document id
+
+  const docRef = doc(db, "userShoppingLists", id);
+  await deleteDoc(docRef);
+  displayShoppingList(email);
+}
+
+async function createShoppingListDocument() {
+  // create a new document, leaving Firestore to allocate its document id automatically
+
+  let newUserPurchase = document.getElementById("newpurchaseitem").value;
+  const collRef = collection(db, "userShoppingLists");
+  const docRef = doc(collRef);
+  await setDoc(docRef, {
+    userEmail: email,
+    userPurchase: newUserPurchase,
+  });
+
+  displayShoppingList(email);
+  document.getElementById("newpurchaseitem").value = "";
+}
